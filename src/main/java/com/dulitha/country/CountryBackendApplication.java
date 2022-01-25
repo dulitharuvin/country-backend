980193c0917ef5cf05866710ab35e75bc5be4c12
Erandi <erandikd@curbfood.com>
2022-01-25 16:03:33 +0100

    initial commit with db connection

---
+++ src/main/java/com/dulitha/country/CountryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dulitha.country;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountryBackendApplication.class, args);
+	}
+
+}
